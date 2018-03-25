0c9e2ca130101d0094af2e810b6399122148a294
Alex Marriott <alex-marriott@live.co.uk>
2018-03-25 15:48:58 +0100

    got the sql create and drop scripts working/.

--- Sql/Create_tables.sql
+++ Sql/Create_tables.sql
@@ -5,63 +5,65 @@
  * The create tables script creates the necessary tables to work in the database. ID's for each table is auto incremented.
  */
 
-CREATE TABLE user_tbl
-(website_user_id INT(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
-email_address VARCHAR(40) NOT NULL UNIQUE,
-account_password VARCHAR(30) NOT NULL,
-username VARCHAR(20) NOT NULL,
-CONSTRAINT website_user_PK PRIMARY KEY (website_user_id));
+CREATE TABLE user
+(user_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+email_address VARCHAR(100) NOT NULL UNIQUE,
+account_password VARCHAR(100) NOT NULL,
+username VARCHAR(100) NOT NULL,
+CONSTRAINT website_user_PK PRIMARY KEY (user_id));
 
-CREATE TABLE categories_tbl
-(category_id INT(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+CREATE TABLE categories
+(category_id BIGINT(20) NOT NULL AUTO_INCREMENT,
 category_name VARCHAR(30) NOT NULL,
 CONSTRAINT category_entree UNIQUE (category_id,category_name),
 CONSTRAINT category_id_PK PRIMARY KEY (category_id));
 
-CREATE TABLE sub_categories_tbl
-(subcategory_id INT(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+CREATE TABLE sub_categories
+(subcategory_id BIGINT(20) NOT NULL AUTO_INCREMENT,
 subcategory_name VARCHAR(100) NOT NULL,
 CONSTRAINT sub_categories_entree UNIQUE (subcategory_id,subcategory_name),
 CONSTRAINT subcategory_id_PK PRIMARY KEY (subcategory_id));
 
-CREATE TABLE ratings_tbl
-(ratings_id INT(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
- rating_score INT(1) NOT NULL UNIQUE,
-  CONSTRAINT rating_id_PK PRIMARY KEY (ratings_id));
+CREATE TABLE ratings
+(ratings_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+ rating_score INT(2) NOT NULL,
+ CONSTRAINT rating_id_PK PRIMARY KEY (ratings_id));
 
-CREATE TABLE comments_tbl
-(comments_id INT(5) unsigned zerofill NOT NULL,
- comment_usename TEXT,
- comment TEXT,
+CREATE TABLE comments
+(comments_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+ comment VARCHAR(256),
  comment_date DATE NOT NULL,
-  CONSTRAINT comment_id_PK PRIMARY KEY (comments_id));
+ comment_user_id BIGINT(20) NOT NULL,
+ CONSTRAINT comment_id_PK PRIMARY KEY (comments_id),
+ CONSTRAINT comment_user_FK FOREIGN KEY (comment_user_id) REFERENCES user (user_id));
 
-CREATE TABLE post_tbl
-(post_id INT(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
+CREATE TABLE post
+(post_id BIGINT(20) NOT NULL AUTO_INCREMENT,
 post_title VARCHAR(100) NOT NULL,
-post_desc VARCHAR(5000) NOT NULL,
+post_body VARCHAR(256) NOT NULL,
 post_date DATE NOT NULL,
-website_user_id_FK INT(5) unsigned zerofill NOT NULL,
-rating_id_FK INT(5) unsigned zerofill NOT NULL,
-comment_id_FK INT(5) unsigned zerofill NOT NULL,
+website_user_id_FK BIGINT(20) NOT NULL,
+rating_id_FK BIGINT(20) NOT NULL,
 CONSTRAINT post_id_PK PRIMARY KEY (post_id),
-CONSTRAINT website_user_id_FK FOREIGN KEY (website_user_id_FK) REFERENCES user_tbl(website_user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT ratings_id_FK FOREIGN KEY (rating_id_FK) REFERENCES ratings_tbl(ratings_id) ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT comment_id_FK FOREIGN KEY (comment_id_FK) REFERENCES comments_tbl(comments_id) ON DELETE CASCADE ON UPDATE CASCADE);
+CONSTRAINT website_user_id_FK FOREIGN KEY (website_user_id_FK) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE);
 
-
-
-CREATE TABLE rating_comment_link
-(rating_id_link_FK INT (5) unsigned zerofill NOT NULL,
-comment_id_link_FK INT (5) unsigned zerofill NOT NULL,
-CONSTRAINT rating_comment_link_PK PRIMARY KEY (rating_id_link_FK,comment_id_link_FK),
-CONSTRAINT rating_id_link_FK FOREIGN KEY (rating_id_link_FK) REFERENCES ratings_tbl (ratings_id),
-CONSTRAINT comment_id_link_FK FOREIGN KEY (comment_id_link_FK) REFERENCES comments_tbl (comments_id));
+CREATE TABLE post_comment_link
+(post_id_link BIGINT(20) NOT NULL,
+comment_id_link BIGINT(20) NOT NULL,
+CONSTRAINT post_comment_link_PK PRIMARY KEY (post_id_link,comment_id_link),
+CONSTRAINT post_id_link_FK FOREIGN KEY (post_id_link) REFERENCES post(post_id),
+CONSTRAINT comment_id_link_FK FOREIGN KEY (comment_id_link) REFERENCES comments(comments_id));
 
 CREATE TABLE cat_subcat_link
-(category_id_link_FK INT (5) unsigned zerofill NOT NULL,
-subcategory_id_link_FK INT (5) unsigned zerofill NOT NULL,
-CONSTRAINT category_id_link_PK PRIMARY KEY (category_id_link_FK,subcategory_id_link_FK),
-CONSTRAINT category_id_link_FK FOREIGN KEY (category_id_link_FK) REFERENCES categories_tbl (category_id),
-CONSTRAINT subcategory_id_link_FK FOREIGN KEY (subcategory_id_link_FK) REFERENCES sub_categories_tbl (subcategory_id));
+(category_id_link BIGINT(20) NOT NULL,
+subcategory_id_link BIGINT(20) NOT NULL,
+CONSTRAINT category_id_link_PK PRIMARY KEY (category_id_link,subcategory_id_link),
+CONSTRAINT category_id_link_FK FOREIGN KEY (category_id_link) REFERENCES categories (category_id),
+CONSTRAINT subcategory_id_link_FK FOREIGN KEY (subcategory_id_link) REFERENCES sub_categories (subcategory_id));
 
+CREATE TABLE rating_comment_link
+(rating_id_link BIGINT(20) NOT NULL,
+ comment_id_link BIGINT(20) NOT NULL,
+ CONSTRAINT rating_comment_link_PK PRIMARY KEY (rating_id_link,comment_id_link),
+ CONSTRAINT rating_comment_link_rating_FK FOREIGN KEY (rating_id_link) REFERENCES ratings (ratings_id),
+ CONSTRAINT rating_comment_link_comment_FK FOREIGN KEY (comment_id_link) REFERENCES comments (comments_id));
--- Sql/Insert_data.sql
+++ Sql/Insert_data.sql
@@ -6,45 +6,69 @@
  */
 
 
-INSERT INTO user_tbl (website_user_id, email_address, account_password,username) VALUES ("alex-blah123@gmail.com", "Blah1234","amarriott");
-INSERT INTO user_tbl (website_user_id, email_address, account_password,username) VALUES ("supercool23@gmail.com", "supplier_name","toomanycooks");
-INSERT INTO user_tbl (website_user_id, email_address, account_password,username) VALUES ("email_address@gmail.com", "123345","whatislife");
+INSERT INTO user (email_address, account_password,username) VALUES ("alex-blah123@gmail.com", "Blah1234","amarriott");
+INSERT INTO user (email_address, account_password,username) VALUES ("supercool23@gmail.com", "supplier_name","toomanycooks");
+INSERT INTO user (email_address, account_password,username) VALUES ("email_address@gmail.com", "123345","whatislife");
 
-INSERT INTO categories_tbl (category_name) VALUES ("Networking");
-INSERT INTO categories_tbl (category_name) VALUES ("Software");
-INSERT INTO categories_tbl (category_name) VALUES ("Infrastructure");
-
-
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Subnetting");
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Classful");
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Classless");
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Java");
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Python");
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Ubuntu");
-INSERT INTO sub_categories_tbl (subcategory_name) VALUES ("Centos");
+INSERT INTO categories (category_name) VALUES ("Networking");
+INSERT INTO categories (category_name) VALUES ("Software");
+INSERT INTO categories (category_name) VALUES ("Infrastructure");
 
 
+INSERT INTO sub_categories (subcategory_name) VALUES ("Subnetting");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Classful");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Classless");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Java");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Ruby");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Python");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Centos");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Ubuntu");
+INSERT INTO sub_categories (subcategory_name) VALUES ("Arch");
 
-INSERT INTO post_tbl (post_title,post_body,post_date) VALUES ("how to fix ","stuff about the post here", "2016-05-21");
-INSERT INTO post_tbl (post_title,post_body,post_date) VALUES ("how to fix ","stuff about the post here", "2016-05-21");
-INSERT INTO post_tbl (post_title,post_body,post_date) VALUES ("how to fix ","stuff about the post here", "2016-05-21");
-INSERT INTO post_tbl (post_title,post_body,post_date) VALUES ("how to fix ","stuff about the post here", "2016-05-21");
-INSERT INTO post_tbl (post_title,post_body,post_date) VALUES ("how to fix ","stuff about the post here", "2016-05-21");
+INSERT INTO ratings (rating_score) VALUES (1);
+INSERT INTO ratings (rating_score) VALUES (2);
+INSERT INTO ratings (rating_score) VALUES (3);
+INSERT INTO ratings (rating_score) VALUES (4);
+INSERT INTO ratings (rating_score) VALUES (5);
+INSERT INTO ratings (rating_score) VALUES (6);
+INSERT INTO ratings (rating_score) VALUES (7);
+INSERT INTO ratings (rating_score) VALUES (8);
+INSERT INTO ratings (rating_score) VALUES (9);
+INSERT INTO ratings (rating_score) VALUES (10);
 
 
-INSERT INTO comments_tbl (customer_id,comment,comment_date) VALUES ("ayyyy lmao good post", "2017-03-12");
-INSERT INTO comments_tbl (customer_id,comment,comment_date) VALUES ("ayyyy lmao good post", "2017-03-12");
-INSERT INTO comments_tbl (customer_id,comment,comment_date) VALUES ("ayyyy lmao good post", "2017-03-12");
-INSERT INTO comments_tbl (customer_id,comment,comment_date) VALUES ("ayyyy lmao good post", "2017-03-12");
+INSERT INTO post (post_title,post_body,post_date,website_user_id_FK) VALUES ("how to fix ","stuff about the post here", "2016-05-21",1);
+INSERT INTO post (post_title,post_body,post_date,website_user_id_FK) VALUES ("how to fix ","stuff about the post here", "2016-05-21",2);
+INSERT INTO post (post_title,post_body,post_date,website_user_id_FK) VALUES ("how to fix ","stuff about the post here", "2016-05-21",1);
+INSERT INTO post (post_title,post_body,post_date,website_user_id_FK) VALUES ("how to fix ","stuff about the post here", "2016-05-21",3);
+INSERT INTO post (post_title,post_body,post_date,website_user_id_FK) VALUES ("how to fix ","stuff about the post here", "2016-05-21",3);
+
+
+INSERT INTO comments (comment,comment_date,comment_user_id) VALUES ("ayyyy lmao good post", "2017-03-12",1);
+INSERT INTO comments (comment,comment_date,comment_user_id) VALUES ("ayyyy lmao good post", "2017-03-12",1);
+INSERT INTO comments (comment,comment_date,comment_user_id) VALUES ("ayyyy lmao good post", "2017-03-12",1);
+INSERT INTO comments (comment,comment_date,comment_user_id) VALUES ("ayyyy lmao good post", "2017-03-12",1);
 
 
 
-INSERT INTO rating_comment_link (rating_id_link_FK,comment_id_link_FK) VALUES ("0001","0001");
-INSERT INTO rating_comment_link (rating_id_link_FK,comment_id_link_FK) VALUES ("0001","0001");
-INSERT INTO rating_comment_link (rating_id_link_FK,comment_id_link_FK) VALUES ("0001","0001");
+INSERT INTO rating_comment_link (rating_id_link,comment_id_link) VALUES (1,1);
+INSERT INTO rating_comment_link (rating_id_link,comment_id_link) VALUES (1,2);
+INSERT INTO rating_comment_link (rating_id_link,comment_id_link) VALUES (1,3);
 
 
 
-INSERT INTO cat_subcat_link (category_id_link_FK,category_id_link_FK) VALUES ("0001", "0001");
-INSERT INTO cat_subcat_link (category_id_link_FK,category_id_link_FK) VALUES ("0001", "0001");
-INSERT INTO cat_subcat_link (category_id_link_FK,category_id_link_FK) VALUES ("0001", "0001");
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (1, 1);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (1, 2);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (1, 3);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (2, 4);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (2, 5);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (2, 6);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (3, 7);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (3, 8);
+INSERT INTO cat_subcat_link (category_id_link,subcategory_id_link) VALUES (3, 9);
+
+
+INSERT INTO post_comment_link (rating_id_link,comment_id_link) VALUES (3,9);
+INSERT INTO post_comment_link (rating_id_link,comment_id_link) VALUES (3, 9);
+INSERT INTO post_comment_link (rating_id_link,comment_id_link) VALUES (3, 9);
+INSERT INTO post_comment_link (rating_id_link,comment_id_link) VALUES (3, 9);
